0cd09126a2dd9e8eda79133a6dcd0e4dfe9903c1
Suryash Chakravarty <suryash.c@gmail.com>
2026-01-02 09:51:27 -0800

    Add initial implementation of 3D scene with LOD support and lighting

---
+++ scripts/main.js
@@ -0,0 +1,113 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { PerformanceMonitor } from './performance_monitor.js'
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor("#262837");
+renderer.setPixelRatio(window.devicePixelRatio);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(40,10,25);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = false;
+controls.enablePan = true;
+controls.minDistance=1;
+controls.maxDistance=100;
+controls.minPolarAngle=0.5;
+controls.maxPolarAngle=1.5;
+controls.autoRotate=false;
+controls.target = new THREE.Vector3(0,0,0);
+controls.update()
+
+const light_2 = new THREE.DirectionalLight(0xffffff, 0.25);
+light_2.position.set(10,10,10)
+scene.add(light_2);
+
+const light_3 = new THREE.DirectionalLight(0xffffff, 0.25);
+light_3.position.set(-10,-10,10)
+scene.add(light_3);
+
+const light_4 = new THREE.DirectionalLight(0xffffff, 0.25);
+light_4.position.set(10,-10,10)
+scene.add(light_4);
+
+const light_5 = new THREE.DirectionalLight(0xffffff, 0.25);
+light_5.position.set(-10,10,10)
+scene.add(light_5);
+
+const gridHelper = new THREE.GridHelper( 100, 50 ); // ( size, divisions )
+scene.add( gridHelper );
+
+const perfMonitor = new PerformanceMonitor()
+    
+const loader = new GLTFLoader();
+
+loader.load('public/models/piperacks_lod.glb', (gltf) => {    
+    const gltfScene = gltf.scene;
+
+    const objects = new Map()
+
+    gltfScene.traverse((child) => {
+        if (child.isMesh) {
+            const [obj, res] = child.name.split(';')
+
+            if (!objects.has(obj)) {
+                objects.set(obj, new Map())
+                objects.get(obj).set(res, child)
+
+            } else {
+                objects.get(obj).set(res, child)
+            }
+        }
+    })
+
+    objects.forEach((res_map, object_id) => {
+        const lod = new THREE.LOD()
+
+        let hires_pos = res_map.get('hires').position
+        let worldPos = new THREE.Vector3();
+        let worldQuat = new THREE.Quaternion();
+        let worldScale = new THREE.Vector3();
+        
+        res_map.get('hires').updateMatrixWorld(true)
+
+        res_map.get('hires').matrixWorld.decompose(worldPos, worldQuat, worldScale);
+        
+        lod.position.copy(hires_pos)
+        lod.quaternion.copy(worldQuat);
+        lod.scale.copy(worldScale);
+
+        res_map.forEach((mesh, resolution) => {
+            mesh.position.set(0, 0, 0);
+            mesh.quaternion.set(0, 0, 0, 1);
+            mesh.scale.set(1, 1, 1);
+
+            if (resolution === 'hires') {
+                lod.addLevel(mesh, 0)
+            } else {
+                lod.addLevel(mesh, 5)
+            }
+        })
+
+        scene.add(lod)
+    })
+})
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+
+    perfMonitor.update(renderer, scene);
+};
+
+animate();
